bbdfc3e803804fb74c4a2ea6625a7b9ac366fa02
Shahzar Mazhar <mohdshahzar.1996@gmail.com>
2023-01-30 13:53:47 +0530

    Added view image feature

--- scripts.js
+++ scripts.js
@@ -10,5 +10,44 @@ jQuery(function($){
       });
     }
 
+    var gallery = $('.the-gallery');
+
+    // check if gallery exists
+    if(gallery.length){
+
+      // add click event to all gallery items
+      gallery.find('.gallery-item').click(function(){
+        var img = $(this).find('img');
+        var src = img.attr('src');
+        var alt = img.attr('alt');
+        var title = img.attr('title');
+
+        $('#single-image-modal').remove();
+
+        // create modal
+        var modal = $('<div />').attr('id', 'single-image-modal').addClass('modal').css('display', 'none');
+        var modalContent = $('<div />').addClass('modal-content');
+        var modalImage = $('<img />').attr('src', src).attr('alt', alt).attr('title', title);
+        var modalClose = $('<i />').addClass('fa fa-multiply close').click(function(){
+          modal.fadeOut(200, function(){
+            modal.remove();
+        });
+
+        });
+
+        // append elements to modal
+        modalContent.append(modalImage);
+        modalContent.append(modalClose);
+        modal.append(modalContent);
+
+        // append modal to body
+        $('body').append(modal);
+
+        // fade in modal
+        modal.fadeIn(200);
+
+    });
+  }
+
 
 });
